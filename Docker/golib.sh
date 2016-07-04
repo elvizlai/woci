d2b95b6edce57fa25375e850b38491335dac374c
Yongzheng Lai <sdrzlyz@126.com>
2016-07-04 18:58:44 +0800

    Dockerfile resize

--- Docker/golib.sh
+++ Docker/golib.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
 echo '-----------go get start-----------'
-go get -u github.com/lib/pq
-go get -u github.com/garyburd/redigo/redis
-go get -u github.com/coreos/etcd/client
-go get -u github.com/nsqio/go-nsq
+go get github.com/golang/protobuf/protoc-gen-go
 
-go get -u golang.org/x/crypto/bcrypt
-go get -u google.golang.org/grpc
-go get -u github.com/golang/protobuf/protoc-gen-go
+go get -d github.com/lib/pq
+go get -d github.com/garyburd/redigo/redis
+go get -d github.com/coreos/etcd/client
+go get -d github.com/nsqio/go-nsq
 
-go get -u github.com/urfave/negroni
-go get -u github.com/gorilla/mux
-go get -u github.com/gorilla/context
-go get -u github.com/dgrijalva/jwt-go
-go get -u github.com/pborman/uuid
+go get -d golang.org/x/crypto/bcrypt
+go get -d google.golang.org/grpc
 
-go get -u github.com/wothing/log
-go get -u github.com/wothing/worc
-go get -u github.com/wothing/wonaming/etcd
+go get -d github.com/urfave/negroni
+go get -d github.com/gorilla/mux
+go get -d github.com/gorilla/context
+go get -d github.com/dgrijalva/jwt-go
+go get -d github.com/pborman/uuid
 
-go get -u github.com/elgs/gostrgen
-go get -u qiniupkg.com/api.v7/kodo
-go get -u qiniupkg.com/x/url.v7
-go get -u github.com/ylywyn/jpush-api-go-client
-go get -u github.com/pingplusplus/pingpp-go/pingpp
-go get -u github.com/tealeg/xlsx
+go get -d github.com/wothing/log
+go get -d github.com/wothing/worc
+go get -d github.com/wothing/wonaming/etcd
 
-go get -u github.com/smartystreets/assertions
-go get -u github.com/smartystreets/goconvey
+go get -d github.com/elgs/gostrgen
+go get -d qiniupkg.com/api.v7/kodo
+go get -d qiniupkg.com/x/url.v7
+go get -d github.com/ylywyn/jpush-api-go-client
+go get -d github.com/pingplusplus/pingpp-go/pingpp
+go get -d github.com/tealeg/xlsx
 
-go get -u github.com/bitly/go-simplejson
+go get -d github.com/smartystreets/assertions
+go get -d github.com/smartystreets/goconvey
+
+go get -d github.com/bitly/go-simplejson
 echo '-----------go get end-----------'
