2ce95545965662cc6c3e024c95f609105e321052
Yongzheng Lai <sdrzlyz@126.com>
2016-07-11 14:05:37 +0800

    fix test

--- ci/test.go
+++ ci/test.go
@@ -18,7 +18,7 @@ import (
 func Test(names ...string) {
 	if len(names) == 0 {
 		for _, v := range conf.Config.Test {
-			data, err := cmd.TCMD("TIEST", strings.Replace(v, "[PLACEHOLDER]", "", -1))
+			data, err := cmd.TCMD("TEEST", strings.Replace(v, "[PLACEHOLDER]", "", -1))
 			if err != nil {
 				log.TErrorORFatal(conf.Config.TRACER, "%v,%v", data, err)
 			}
@@ -27,7 +27,7 @@ func Test(names ...string) {
 	} else {
 		for _, name := range names {
 			for _, v := range conf.Config.Test {
-				data, err := cmd.TCMD("TEST", strings.Replace(v, "[PLACEHOLDER]", name, -1))
+				data, err := cmd.TCMD("TEEST", strings.Replace(v, "[PLACEHOLDER]", name, -1))
 				if err != nil {
 					log.TErrorORFatal(conf.Config.TRACER, "%v,%v", data, err)
 				}
